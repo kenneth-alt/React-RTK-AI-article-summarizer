fe983f295f4a929294589b7162c37ca0e2fadff9
kenneth-alt <kenneth.ugo15@gmail.com>
2023-09-06 21:42:30 -0600

    add Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,25 +1,76 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 
 import { copy, linkIcon, loader, tick } from '../assets'
+import { useLazyGetSummaryQuery } from "../services/article"
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  })
+
+  const [allArticles, setAllArticles] = useState([])
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
+
+    if(articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, [])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const { data } = await getSummary({ articleUrl: article.url })
+
+    if(data?.summary) {
+      const newArticle = { ...article, summary: data.summary }
+      const updatedAllArticles = [newArticle, ...allArticles]
+
+      setArticle(newArticle)
+      setAllArticles(updatedAllArticles)
+
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
+    }
+  }
+
+
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* search */}
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center" onSubmit={() => {}}>
+        <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
           <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5" />
 
-          <input type="url" placeholder="Enter your article url..." value="" onChange={() => {}} required className="url_input peer" />
+          <input type="url" placeholder="Enter your article url..." value={article.url} onChange={(e) => setArticle({ ...article, url: e.target.value })} required className="url_input peer" />
 
           <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">
             &crarr;
           </button>
         </form>
 
-        
+
+        {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+
+            <div className="link_card" >
+
+              <div className="copy_btn">
+                <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain" />
+              </div>
+
+            </div>
+            
+        </div>
       </div>
+
+      {/* Display Results */}
+
+  
     </section>
+    
   )
 }
 
